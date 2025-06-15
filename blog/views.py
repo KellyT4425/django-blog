3bc53ce205a239b7860fd6963dea2324c1cfa619
KellyT4425 <k.thompson4421@yahoo.com>
2025-06-15 19:17:01 +0100

    templates, inheritance, tags etc

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,5 @@ class PostList(generic.ListView):
     # queryset = Post.objects.filter(author=2)  # filtering by author.
     # queryset = Post.objects.all().order_by("created_on") -created_on orders in reverse.
     queryset = Post.objects.filter(status=1)
-    template_name = 'post_list.html'
+    template_name = "blog/index.html"
+    paginate_by = 6
